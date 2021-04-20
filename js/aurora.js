6dda0bc7ddfa6fee3bbefc712b82cb383c036b8f
tang <2553168654@qq.com>
2021-04-20 14:09:51 +0800

    Site updated: 2021-04-20 14:09:45

--- js/aurora.js
+++ js/aurora.js
@@ -5,9 +5,7 @@ var top_up =
 document.getElementById('go-up').innerHTML += top_up
 /* 手机客户端导航栏默认隐藏 */
 var mobile_sidebar_menus = document.getElementById('mobile-sidebar-menus')
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
-	'menus_item_child'
-)
+var menus_item_child = document.getElementsByClassName('menus_item_child')
 
 var menus_expand = mobile_sidebar_menus.getElementsByClassName('expand')
 for (var i = 0; i < menus_item_child.length; i++) {
@@ -20,7 +18,7 @@ var titleTime
 document.addEventListener('visibilitychange', function () {
 	if (document.hidden) {
 		$('[rel="icon"]').attr('href', '/img/favicon.ico')
-		document.title = '(つェ⊂) 我藏好了哦~~'
+		document.title = '(つェ⊂) Hide On Bush~~'
 		clearTimeout(titleTime)
 	} else {
 		$('[rel="icon"]').attr('href', '/img/favicon.ico')
